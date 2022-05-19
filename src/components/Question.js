abf5c9c4fc143a12640dfd2161a567183bddfcde
kenyansa <kenyansajustine@gmail.com>
2022-05-19 08:36:56 +0300

    submit question.js and app.js files with all the needed functions

---
+++ src/components/Question.js
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from "react";
+
+function Question({ question, onAnswered }) {
+  const [timeRemaining, setTimeRemaining] = useState(10);
+
+  // add useEffect code
+  //creating a side effect using useEffect and using setTimeout to run a callback function after 1 second.
+  useEffect(() => {
+    if (timeRemaining === 0) {
+      //When timeRemaining hits 0, reset timeRemaining back to 10 seconds, so that our next question will have a fresh timer.
+      setTimeRemaining(10);
+      onAnswered(false);
+      return;
+    }
+
+    // setting up a timeout to run after 1 second
+    const timerId = setTimeout(() => {
+      // decrement the time remaining
+      setTimeRemaining((timeRemaining) => timeRemaining - 1);
+    }, 1000);
+
+    // clean up after the timeout in case the component unmounts before the timer is done
+    return function () {
+      clearTimeout(timerId);
+    };
+  }, [timeRemaining, onAnswered]);
+  // we want to run the effect every time timeRemaining changes
+  // onAnswered is also a dependency, even though it doesn't change
+
+  function handleAnswer(isCorrect) {
+    setTimeRemaining(10);
+    onAnswered(isCorrect);
+  }
+
+  const { id, prompt, answers, correctIndex } = question;
+
+  return (
+    <>
+      <h1>Question {id}</h1>
+      <h3>{prompt}</h3>
+      {answers.map((answer, index) => {
+        const isCorrect = index === correctIndex;
+        return (
+          <button key={answer} onClick={() => handleAnswer(isCorrect)}>
+            {answer}
+          </button>
+        );
+      })}
+      <h5>{timeRemaining} seconds remaining</h5>
+    </>
+  );
+}
+
+export default Question;
